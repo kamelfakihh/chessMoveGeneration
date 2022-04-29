350cf52f6c86f36afdeb765219586c2771972993
kamelfakihh <kamelfakihh@gmail.com>
2022-04-29 19:23:56 +0300

    defined files and positions indices and fixed white king and queen initial positions

--- include/Definitions.h
+++ include/Definitions.h
@@ -4,15 +4,92 @@
 #include <cstdint>
 
 // define bitboard as a 64bit unsigned number
-typedef uint64_t bitboard;
+typedef unsigned long long bitboard;
+
+// position/square index
+
+#define A1 0
+#define B1 1
+#define C1 2
+#define D1 3
+#define E1 4
+#define F1 5
+#define G1 6
+#define H1 7
+#define A2 8
+#define B2 9
+#define C2 10
+#define D2 11
+#define E2 12
+#define F2 13
+#define G2 14
+#define H2 15
+#define A3 16
+#define B3 17
+#define C3 18
+#define D3 19
+#define E3 20
+#define F3 21
+#define G3 22
+#define H3 23
+#define A4 24
+#define B4 25
+#define C4 26
+#define D4 27
+#define E4 28
+#define F4 29
+#define G4 30
+#define H4 31
+#define A5 32
+#define B5 33
+#define C5 34
+#define D5 35
+#define E5 36
+#define F5 37
+#define G5 38
+#define H5 39
+#define A6 40
+#define B6 41
+#define C6 42
+#define D6 43
+#define E6 44
+#define F6 45
+#define G6 46
+#define H6 47
+#define A7 48
+#define B7 49
+#define C7 50
+#define D7 51
+#define E7 52
+#define F7 53
+#define G7 54
+#define H7 55
+#define A8 56
+#define B8 57
+#define C8 58
+#define D8 59
+#define E8 60
+#define F8 61
+#define G8 62
+#define H8 63
+
+// File index 
+#define FILE_A 0
+#define FILE_B 1
+#define FILE_C 2
+#define FILE_D 3
+#define FILE_E 4 
+#define FILE_F 5 
+#define FILE_G 6 
+#define FILE_H 7
 
 // initial positions bitboards
 const bitboard whitePawnsInitial    = 0b0000000000000000000000000000000000000000000000001111111100000000;
 const bitboard whiteRooksInitial    = 0b0000000000000000000000000000000000000000000000000000000010000001;
 const bitboard whiteKnightsInitial  = 0b0000000000000000000000000000000000000000000000000000000001000010;
 const bitboard whiteBishopsInitial  = 0b0000000000000000000000000000000000000000000000000000000000100100;
-const bitboard whiteQueensInitial   = 0b0000000000000000000000000000000000000000000000000000000000010000;
-const bitboard whiteKingInitial     = 0b0000000000000000000000000000000000000000000000000000000000001000;
+const bitboard whiteQueensInitial   = 0b0000000000000000000000000000000000000000000000000000000000001000;
+const bitboard whiteKingInitial     = 0b0000000000000000000000000000000000000000000000000000000000010000;
 const bitboard blackPawnsInitial    = 0b0000000011111111000000000000000000000000000000000000000000000000;
 const bitboard blackRooksInitial    = 0b1000000100000000000000000000000000000000000000000000000000000000;
 const bitboard blackKnightsInitial  = 0b0100001000000000000000000000000000000000000000000000000000000000;
